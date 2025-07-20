b2bae9e4482bff9dbe733e8cccab69dd397da468
shubhamkr-05 <shubhamkragarwal0511@gmail.com>
2025-07-20 11:02:28 +0530

    ..

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL:"https://api-youtube-134a.onrender.com",
-  baseURL:"http://localhost:8000",
+  baseURL:"https://api-youtube-134a.onrender.com",
+  //baseURL:"http://localhost:8000",
   withCredentials: true, // only if you're using cookies
 });
 
